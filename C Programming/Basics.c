3d815e9fc406fd3f53ec65ea78a3aac737cb83da
Jasmeet Singh Arora <jasspn2014@gmail.com>
2020-10-02 15:34:41 +0530

    Added basics to CProgramming

--- C Programming/Basics.c
+++ C Programming/Basics.c
@@ -2,5 +2,42 @@
 
 int main()
 {
-  printf("Hello World!");
+
+  // int i = 5;
+  // float f = 1234567890.32;// Not Valid Ronge Overflow
+  // double d = 1234567890.36;
+  // char c = 'A';
+  // printf("int : %d and size = %d",i,sizeof(i));
+  // printf("\nfloat : %f and size = %d",f,sizeof(f));
+  // printf("\ndouble : %lf and size = %d",d,sizeof(d));
+  // printf("\nchar : %c and size = %d",c,sizeof(c));
+
+
+  // scanf("%s",str);
+  // gets(str);
+
+  // char name[100];
+  // int age;
+  //
+  // printf("\nEnter Your Age: ");
+  // scanf("%d",&age);
+  //
+  // fflush(stdin);
+  // printf("\nEnter your Name: ");
+  // scanf("%[^\n]%*c",name);
+  //
+  // printf("\nName: %s",name);
+  // printf("\nAge: %d",age);
+
+  // Print in a string with " " within the string with the help of backslash (\);
+  // backslash leans backward
+  // forward slash leans forward
+
+  //printf("\"Hello World\"");
+
+  int a = -10;
+  int b = 10;
+
+  printf("%d",a && b); // True because both are true // Only Zero is false rest all integers including negative are true;
+
 }
